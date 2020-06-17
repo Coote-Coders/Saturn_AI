9e9d26561ccc605db066e545e6a40d652fd76669
Nick Kitchel <nkitchel@frontier.com>
2020-06-17 10:24:27 -0700

    Changed email format so the user can just say a name.

--- email_database.py
+++ email_database.py
@@ -1,16 +1,31 @@
 #Email database where it draws the correct emails
 
-email_list['Ben'] = bennybomb@frontier.com
-email_list['Benny'] = bennybomb@frontier.com
-email_list['Ethan'] = ethanodbe@gmail.com
-email_list['Georgie'] = georgiecoote@hotmail.com
-email_list['Grandma Kay'] = kaykitmail@gmail.com
-email_list['Lucy'] = lucycoote@hotmail.com
-email_list['Mom'] = suzyck@frontier.com
-email_list['Max'] = maxmarzolf11@gmail.com
-email_list['Nick'] = nkitchel2000@hotmail.com
-email_list['Opa'] = gerry_lander@hotmail.com
-email_list['Roger'] = rcoote@microsoft.com
-email_list['Paul'] = pwarnick@uidaho.edu
-email_list['Katie Coote'] = katiecoote@hotmail.com
-email_list['Katie'] = katiekitchel23@gmail.com
+email_person[1] = "Ben"
+email_person[2]= "Benny"
+email_person[3] = "Ethan"
+email_person[4] = "Georgie"
+email_person[5] = "Grandma Kay"
+email_person[6] = "Lucy"
+email_person[7] = "Mom"
+email_person[8] = "Max"
+email_person[9] = "Nick"
+email_person[10] = "Opa"
+email_person[11] = "Roger"
+email_person[12] = "Paul"
+email_person[13] = "Katie Coote"
+email_person[14] = "Katie"
+
+email_list[1] = bennybomb@frontier.com
+email_list[2] = bennybomb@frontier.com
+email_list[3] = ethanodbe@gmail.com
+email_list[4] = georgiecoote@hotmail.com
+email_list[5] = kaykitmail@gmail.com
+email_list[6] = lucycoote@hotmail.com
+email_list[7] = suzyck@frontier.com
+email_list[8] = maxmarzolf11@gmail.com
+email_list[9] = nkitchel2000@hotmail.com
+email_list[10] = gerry_lander@hotmail.com
+email_list[11] = rcoote@microsoft.com
+email_list[12] = pwarnick@uidaho.edu
+email_list[13] = katiecoote@hotmail.com
+email_list[14] = katiekitchel23@gmail.com
