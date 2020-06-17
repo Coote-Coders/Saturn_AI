e913616f1e60a8415ed338e429a65340582fd75e
Nick Kitchel <nkitchel@frontier.com>
2020-06-16 20:45:33 -0700

    Added emails to the database.

--- email_database.py
+++ email_database.py
@@ -1,1 +1,16 @@
 #Email database where it draws the correct emails
+
+email_list['Ben'] = bennybomb@frontier.com
+email_list['Benny'] = bennybomb@frontier.com
+email_list['Ethan'] = ethanodbe@gmail.com
+email_list['Georgie'] = georgiecoote@hotmail.com
+email_list['Grandma Kay'] = kaykitmail@gmail.com
+email_list['Lucy'] = lucycoote@hotmail.com
+email_list['Mom'] = suzyck@frontier.com
+email_list['Max'] = maxmarzolf11@gmail.com
+email_list['Nick'] = nkitchel2000@hotmail.com
+email_list['Opa'] = gerry_lander@hotmail.com
+email_list['Roger'] = rcoote@microsoft.com
+email_list['Paul'] = pwarnick@uidaho.edu
+email_list['Katie Coote'] = katiecoote@hotmail.com
+email_list['Katie'] = katiekitchel23@gmail.com
