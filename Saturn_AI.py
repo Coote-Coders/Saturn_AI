6785f82dbfaf26a71bee12c4bef6b8ec4f10737e
Nick Kitchel <nkitchel@frontier.com>
2020-06-17 14:16:13 -0700

    Worked on email and app opening features.

--- Saturn_AI.py
+++ Saturn_AI.py
@@ -12,6 +12,13 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 import email_database
+import application_database
+from email_database import email_person
+from email_database import email_list
+from application_database import app_location
+from application_database import application_name
+
+os.startfile(app_location[6])
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
 LANGUAGE = "en-US,en;q=0.5"
--- email_database.py
+++ email_database.py
@@ -1,5 +1,8 @@
 #Email database where it draws the correct emails
 
+email_person = {}
+email_list = {}
+
 email_person[1] = "Ben"
 email_person[2]= "Benny"
 email_person[3] = "Ethan"
@@ -15,17 +18,17 @@ email_person[12] = "Paul"
 email_person[13] = "Katie Coote"
 email_person[14] = "Katie"
 
-email_list[1] = bennybomb@frontier.com
-email_list[2] = bennybomb@frontier.com
-email_list[3] = ethanodbe@gmail.com
-email_list[4] = georgiecoote@hotmail.com
-email_list[5] = kaykitmail@gmail.com
-email_list[6] = lucycoote@hotmail.com
-email_list[7] = suzyck@frontier.com
-email_list[8] = maxmarzolf11@gmail.com
-email_list[9] = nkitchel2000@hotmail.com
-email_list[10] = gerry_lander@hotmail.com
-email_list[11] = rcoote@microsoft.com
-email_list[12] = pwarnick@uidaho.edu
-email_list[13] = katiecoote@hotmail.com
-email_list[14] = katiekitchel23@gmail.com
+email_list[1] = "bennybomb@frontier.com"
+email_list[2] = "bennybomb@frontier.com"
+email_list[3] = "ethanodbe@gmail.com"
+email_list[4] = "georgiecoote@hotmail.com"
+email_list[5] = "kaykitmail@gmail.com"
+email_list[6] = "lucycoote@hotmail.com"
+email_list[7] = "suzyck@frontier.com"
+email_list[8] = "maxmarzolf11@gmail.com"
+email_list[9] = "nkitchel2000@hotmail.com"
+email_list[10] = "gerry_lander@hotmail.com"
+email_list[11] = "rcoote@microsoft.com"
+email_list[12] = "pwarnick@uidaho.edu"
+email_list[13] = "katiecoote@hotmail.com"
+email_list[14] = "katiekitchel23@gmail.com"
