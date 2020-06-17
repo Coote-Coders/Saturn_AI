4a3d9373258d72fd4ab207a292f28aee75f24cad
Nick Kitchel <nkitchel@frontier.com>
2020-06-16 20:16:25 -0700

    Adjusted file to work with email_database.py

--- Saturn_AI.py
+++ Saturn_AI.py
@@ -11,6 +11,8 @@ import argparse
 import requests
 from bs4 import BeautifulSoup as bs
 
+import email_database
+
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
 LANGUAGE = "en-US,en;q=0.5"
 
