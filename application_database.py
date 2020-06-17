96c465da1eef6b9b3f1a6c8786cf7b99e1f6a731
Nick Kitchel <nkitchel@frontier.com>
2020-06-17 10:43:42 -0700

    Added a file for the database of all the application names.

---
+++ application_database.py
@@ -0,0 +1,14 @@
+#Database for all the application paths that are on the machine
+
+application_name[1] = "Starcraft"
+application_name[1] = "HearthStone"
+application_name[1] = "Photoshop"
+application_name[1] = "Spotify"
+application_name[1] = "Word"
+application_name[1] = "Excel"
+application_name[1] = "InDesign"
+application_name[1] = "Illustrator"
+application_name[1] = "...finish later"
+application_name[1] = "Starcraft"
+application_name[1] = "Starcraft"
+application_name[1] = "Starcraft"
